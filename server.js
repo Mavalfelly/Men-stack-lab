06523c4ef0a713a19ff83a2d928096f9f685d39f
MaValFelly <matthewfeliciano16@gmail.com>
2024-08-13 20:00:19 -0400

    edit and delete frame work

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require('express');
 const Teams = require('./models/teams');
 const methodOverride = require('method-override')
 const app = express();
+//////////////////////////////////////////////
+//////// Middles: Section          //////// 
+///////////////////////////////////////////////
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'));
-
+//////////////////////////////////////////////
+//////// Routes: Section          //////// 
+///////////////////////////////////////////////
 app.get('/teams', async (req,res) =>{
     try{
         let listOfTeams = await Teams.find({});
@@ -20,7 +25,7 @@ app.get('/teams/new',(req, res)=> {
     res.render('new.ejs')
 });
 
-app.post('/teams', async (res,req) =>{
+app.post('/teams', async (req,res) =>{
     try{
         req.body.yearCreated = Number(req.body.yearCreated);
         await Teams.create(req.body)
@@ -29,6 +34,12 @@ app.post('/teams', async (res,req) =>{
         res.status(400).json(err);
     }
 });
+app.get('/teams/:id', async (req,res) =>{
+    const selectedTeam = await Teams.findById(req.params.id);
+    res.render('info.ejs', {team: selectedTeam})
+});
+
+
 
 
 
